eb564fca5de38a604c66e3ed2d47350d624d9150
0118Add <60157610+0118Add@users.noreply.github.com>
2025-08-28 17:10:12 +0800

    Update diy-part3.sh

--- custom/cmiot_ax18/plus/diy-part3.sh
+++ custom/cmiot_ax18/plus/diy-part3.sh
@@ -35,7 +35,7 @@ function merge_feed(){
 rm -rf package/custom; mkdir package/custom
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 
 # 添加删除软件
 rm -rf feeds/packages/net/{chinadns-ng,hysteria,xray-core,v2ray-core,v2ray-geodata,sing-box,shadowsocks-rust,shadowsocksr-libev}
@@ -43,20 +43,9 @@ rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-openclash
-#git clone https://github.com/0118Add/luci-app-vssr package/luci-app-vssr
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/sing-box
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/tuic-client
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/chinadns-ng
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/hysteria
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/shadowsocksr-libev
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/v2ray-core
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/v2ray-geodata
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/shadowsocks-rust
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/xray-core
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/shadow-tls
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/luci-app-homeproxy
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/luci-app-nikki
-merge_package https://github.com/sbwml/openwrt_helloworld openwrt_helloworld/nikki
+
+git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki  package/OpenWrt-nikki
 
 # 去掉ssr+中shadowsocksr-libev的libopenssl-legacy依赖支持
 sed -i 's/ +libopenssl-legacy//g' package/custom/shadowsocksr-libev/Makefile
