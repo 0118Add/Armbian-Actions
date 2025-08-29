e7c51a1e8c42e97017c5cd54803d13288c857340
0118Add <60157610+0118Add@users.noreply.github.com>
2025-08-29 17:06:14 +0800

    Update diy-part3.sh

--- custom/cmiot_ax18/plus/diy-part3.sh
+++ custom/cmiot_ax18/plus/diy-part3.sh
@@ -18,12 +18,12 @@ sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 #rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf feeds/luci/applications/luci-app-smartdns
 #rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-#rm -rf feeds/luci/applications/luci-app-openclash
+#rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-openclash
 
-#git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/OpenClash
+git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/OpenClash
 #git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
-#git clone https://github.com/nikkinikki-org/OpenWrt-momo  package/OpenWrt-momo
+git clone https://github.com/nikkinikki-org/OpenWrt-momo  package/OpenWrt-momo
 
 # 去掉ssr+中shadowsocksr-libev的libopenssl-legacy依赖支持
 #sed -i 's/ +libopenssl-legacy//g' package/custom/shadowsocksr-libev/Makefile
