57fe45aafa35c4084b02dbad8e87889b4672a4db
0118Add <60157610+0118Add@users.noreply.github.com>
2025-08-29 09:06:38 +0800

    Update diy-part3.sh

--- custom/cmiot_ax18/plus/diy-part3.sh
+++ custom/cmiot_ax18/plus/diy-part3.sh
@@ -14,15 +14,15 @@
 sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 
 # 添加删除软件
-rm -rf feeds/packages/net/{ddns-go,chinadns-ng,hysteria,xray-core,v2ray-core,v2ray-geodata,sing-box,shadowsocks-rust,shadowsocksr-libev}
-rm -rf feeds/luci/applications/luci-app-ddns-go
+#rm -rf feeds/packages/net/{ddns-go,chinadns-ng,hysteria,xray-core,v2ray-core,v2ray-geodata,sing-box,shadowsocks-rust,shadowsocksr-libev}
+#rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/luci/applications/luci-app-passwall
+#rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-openclash
+#rm -rf feeds/luci/applications/luci-app-openclash
 
-git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/OpenClash
-git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+#git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/OpenClash
+#git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
 #git clone https://github.com/nikkinikki-org/OpenWrt-momo  package/OpenWrt-momo
 
 # 去掉ssr+中shadowsocksr-libev的libopenssl-legacy依赖支持
